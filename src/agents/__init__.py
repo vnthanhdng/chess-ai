0cdab8054ede4e5bbe707651c88fad3608532cfa
JPark1023 <jaimespark@gmail.com>
2025-11-23 15:49:57 -0600

    Add value and qlearning agents to init

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -2,6 +2,8 @@
 from .base_agent import BaseAgent
 from .human_agent import HumanAgent
 from .search_agent import SearchAgent, MinimaxAgent, AlphaBetaAgent, ExpectimaxAgent
+from .valueIteration_agent import ValueIterationAgent
+from .qlearning_agent import QLearningAgent
 
 __all__ = [
     'BaseAgent',
@@ -9,5 +11,7 @@ __all__ = [
     'SearchAgent',
     'MinimaxAgent',
     'AlphaBetaAgent',
-    'ExpectimaxAgent'
+    'ExpectimaxAgent',
+    'ValueIterationAgent',
+    'QLearningAgent'
 ]
